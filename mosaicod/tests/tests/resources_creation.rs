ec31d8f63ff60ae556d311f2671535f731180a33
Matteo Guidetti <matteo.guidetti@mosaico.dev>
2026-02-17 17:15:17 +0100

    Created test for topic_create action

--- mosaicod/tests/tests/resources_creation.rs
+++ mosaicod/tests/tests/resources_creation.rs
@@ -4,7 +4,7 @@ use mosaicod_repo as repo;
 use tests::{self, actions, common};
 
 #[sqlx::test(migrator = "mosaicod_repo::testing::MIGRATOR")]
-fn sequence_create(pool: sqlx::Pool<repo::Database>) -> sqlx::Result<()> {
+async fn sequence_create(pool: sqlx::Pool<repo::Database>) -> sqlx::Result<()> {
     let port = common::random_port();
 
     let server = common::Server::new(common::HOST, port, pool).await;
@@ -18,7 +18,7 @@ fn sequence_create(pool: sqlx::Pool<repo::Database>) -> sqlx::Result<()> {
 }
 
 #[sqlx::test(migrator = "mosaicod_repo::testing::MIGRATOR")]
-fn session_create(pool: sqlx::Pool<repo::Database>) -> sqlx::Result<()> {
+async fn session_create(pool: sqlx::Pool<repo::Database>) -> sqlx::Result<()> {
     let port = common::random_port();
 
     let server = common::Server::new(common::HOST, port, pool).await;
@@ -34,18 +34,21 @@ fn session_create(pool: sqlx::Pool<repo::Database>) -> sqlx::Result<()> {
     Ok(())
 }
 
-// #[sqlx::test(migrator = "mosaicod_repo::testing::MIGRATOR")]
-// fn topic_create(pool: sqlx::Pool<repo::Database>) -> sqlx::Result<()> {
-//     let port = common::random_port();
-//     let server = common::Server::new(common::HOST, port, pool).await;
-//
-//     let mut client = common::Client::new(common::HOST, port).await;
-//
-//     actions::sequence_create(&mut client, "test_sequence", None).await;
-//
-//     // let key = actions::topic_create(&mut client, &key, "test_sequence/my_topic", None).await;
-//     // assert!(valid_key(&key));
-//     //
-//     server.shutdown().await;
-//     Ok(())
-// }
+#[sqlx::test(migrator = "mosaicod_repo::testing::MIGRATOR")]
+async fn topic_create(pool: sqlx::Pool<repo::Database>) -> sqlx::Result<()> {
+    let port = common::random_port();
+
+    let server = common::Server::new(common::HOST, port, pool).await;
+    let mut client = common::Client::new(common::HOST, port).await;
+
+    let sequence_name = "test_sequence";
+
+    actions::sequence_create(&mut client, sequence_name, None).await;
+    let uuid = actions::session_create(&mut client, sequence_name).await;
+    assert!(uuid.is_valid());
+    let uuid = actions::topic_create(&mut client, &uuid, "test_sequence/my_topic", None).await;
+    assert!(uuid.is_valid());
+
+    server.shutdown().await;
+    Ok(())
+}
