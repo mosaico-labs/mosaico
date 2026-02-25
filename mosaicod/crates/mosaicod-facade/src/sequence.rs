e1afc9eacf6ec698caa120d3d2e2fed44311e625
Matteo Guidetti <matteo.guidetti@mosaico.dev>
2026-02-25 17:06:17 +0100

    Created test for facade::Sequence notify()/notify_purge()

--- mosaicod/crates/mosaicod-facade/src/sequence.rs
+++ mosaicod/crates/mosaicod-facade/src/sequence.rs
@@ -229,11 +229,12 @@ impl Sequence {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use mosaicod_core::types::NotifyType;
 
     use types::{MetadataBlob, Resource};
 
     #[sqlx::test(migrator = "db::testing::MIGRATOR")]
-    fn sequence_creation(pool: sqlx::Pool<db::DatabaseType>) -> sqlx::Result<()> {
+    async fn sequence_creation(pool: sqlx::Pool<db::DatabaseType>) -> sqlx::Result<()> {
         let database = db::testing::Database::new(pool);
         let store = store::testing::Store::new_random_on_tmp().unwrap();
         let fsequence = Sequence::new(
@@ -276,5 +277,79 @@ mod tests {
         );
 
         Ok(())
+    }
+
+    #[sqlx::test(migrator = "db::testing::MIGRATOR")]
+    async fn sequence_notify_and_notify_purge(pool: sqlx::Pool<db::DatabaseType>) {
+        let database = db::testing::Database::new(pool);
+        let store = store::testing::Store::new_random_on_tmp().unwrap();
+        let fsequence = Sequence::new(
+            "test_sequence".to_string(),
+            (*store).clone(),
+            (*database).clone(),
+        );
+
+        fsequence
+            .create(None)
+            .await
+            .expect("Error creating sequence");
+
+        // Check if sequence was created
+        let mut cx = database.connection();
+        let sequence = db::sequence_find_by_locator(&mut cx, &fsequence.locator)
+            .await
+            .expect("Unable to find the created sequence");
+
+        // Check sequence locator
+        assert_eq!(
+            fsequence.locator.path().to_string_lossy(),
+            sequence.locator_name
+        );
+
+        fsequence
+            .notify(NotifyType::Error, "test notify message".to_owned())
+            .await
+            .expect("Error creating notify message");
+
+        fsequence
+            .notify(NotifyType::Error, "test notify message 2".to_owned())
+            .await
+            .expect("Error creating notify message");
+
+        // Check if notifications were created on database.
+        let notifications = db::sequence_notifies_find_by_name(&mut cx, &fsequence.locator)
+            .await
+            .unwrap();
+
+        assert_eq!(notifications.len(), 2);
+
+        let first_notification = notifications.first().unwrap();
+        assert_eq!(
+            first_notification.msg.as_ref().unwrap(),
+            "test notify message"
+        );
+        assert!(first_notification.uuid().is_valid());
+        assert_eq!(first_notification.sequence_id, sequence.sequence_id);
+
+        let second_notification = notifications.last().unwrap();
+        assert_eq!(
+            second_notification.msg.as_ref().unwrap(),
+            "test notify message 2"
+        );
+        assert!(second_notification.uuid().is_valid());
+        assert_eq!(second_notification.sequence_id, sequence.sequence_id);
+
+        fsequence
+            .notify_purge()
+            .await
+            .expect("Unable to purge notifications");
+
+        // Check there are no more notifications on database.
+        assert!(
+            db::sequence_notifies_find_by_name(&mut cx, &fsequence.locator)
+                .await
+                .unwrap()
+                .is_empty()
+        );
     }
 }
