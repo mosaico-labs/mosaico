44cc6a936dea98b7150eb6a9e9e9f435c0f7441a
Francesco Di Corato <francesco.dicorato@dilica.it>
2026-03-19 18:01:17 +0100

    Write integration tests for TopicLevelErrorPolicy. Closes #235

--- mosaico-sdk-py/src/testing/integration/test_error_policy.py
+++ mosaico-sdk-py/src/testing/integration/test_error_policy.py
@@ -4,7 +4,14 @@ import pytest
 
 from mosaicolabs.comm import MosaicoClient
 from mosaicolabs.comm.notifications import NotificationType
-from mosaicolabs.enum import OnErrorPolicy, SessionLevelErrorPolicy
+from mosaicolabs.enum import (
+    OnErrorPolicy,
+    SessionLevelErrorPolicy,
+    TopicLevelErrorPolicy,
+)
+from mosaicolabs.enum.topic_writer_status import TopicWriterStatus
+from mosaicolabs.models.data.geometry import Vector3d
+from mosaicolabs.models.message import Message
 from mosaicolabs.models.sensors import IMU
 
 
@@ -73,6 +80,270 @@ def test_sequence_report(_client: MosaicoClient):
     _client.close()
 
 
+def test_topic_level_error_policy_finalize(_client: MosaicoClient):
+    sequence_name = "sequence-to-delete"
+    topic_name = "/topic1"
+
+    # It is necessary to make the exception propagate until the SequenceWriter.__exit__
+    # which triggers the delete condition
+    with _client.sequence_create(
+        sequence_name, {}, on_error=SessionLevelErrorPolicy.Delete
+    ) as wseq:
+        log.info("Expected one (1) error after this line...")
+        twriter = wseq.topic_create(
+            topic_name, {}, IMU, on_error=TopicLevelErrorPolicy.Finalize
+        )
+        assert twriter is not None
+        with twriter:
+            # raise and exception to exit the context
+            raise Exception("__exception_in_test__")
+        assert twriter.status == TopicWriterStatus.FinalizedWithError
+        assert twriter.last_error == "__exception_in_test__"
+        assert twriter.is_active is False
+
+        # cannot push data
+        with pytest.raises(Exception, match="called on uninitialized state"):
+            twriter.push(
+                Message(
+                    timestamp_ns=0,
+                    data=IMU(
+                        acceleration=Vector3d(x=0, y=0, z=0),
+                        angular_velocity=Vector3d(x=0, y=0, z=0),
+                    ),
+                )
+            )
+        # The last push reset the error
+        assert twriter.status == TopicWriterStatus.RaisedException
+        assert "called on uninitialized state" in twriter.last_error
+
+    shandler = _client.sequence_handler(sequence_name)
+    assert shandler is not None
+    thandler = shandler.get_topic_handler(topic_name)
+    assert thandler is not None
+    assert thandler.locked is True
+
+    # free resources
+    _client.sequence_delete(sequence_name)
+    _client.close()
+
+
+def test_topic_level_error_policy_finalize_multi_topic(_client: MosaicoClient):
+    sequence_name = "sequence-to-delete"
+    topic_name_1 = "/topic1"
+    topic_name_2 = "/topic2"
+
+    # It is necessary to make the exception propagate until the SequenceWriter.__exit__
+    # which triggers the delete condition
+    with _client.sequence_create(
+        sequence_name, {}, on_error=SessionLevelErrorPolicy.Delete
+    ) as wseq:
+        log.info("Expected one (1) error after this line...")
+        twriter_1 = wseq.topic_create(
+            topic_name_1, {}, IMU, on_error=TopicLevelErrorPolicy.Finalize
+        )
+        twriter_2 = wseq.topic_create(
+            topic_name_2, {}, IMU, on_error=TopicLevelErrorPolicy.Finalize
+        )
+        assert twriter_1 is not None
+        assert twriter_2 is not None
+        with twriter_1:
+            # raise and exception to exit the context
+            raise Exception("__exception_in_test__")
+        assert twriter_1.status == TopicWriterStatus.FinalizedWithError
+        assert twriter_1.last_error == "__exception_in_test__"
+        assert twriter_1.is_active is False
+
+        # cannot push data
+        with pytest.raises(Exception, match="called on uninitialized state"):
+            twriter_1.push(
+                Message(
+                    timestamp_ns=0,
+                    data=IMU(
+                        acceleration=Vector3d(x=0, y=0, z=0),
+                        angular_velocity=Vector3d(x=0, y=0, z=0),
+                    ),
+                )
+            )
+        # The last push reset the error
+        assert twriter_1.status == TopicWriterStatus.RaisedException
+        assert "called on uninitialized state" in twriter_1.last_error
+
+        # Demonstrate topic independence
+        with twriter_2:
+            # This is ok and is successfull
+            twriter_2.push(
+                Message(
+                    timestamp_ns=0,
+                    data=IMU(
+                        acceleration=Vector3d(x=0, y=0, z=0),
+                        angular_velocity=Vector3d(x=0, y=0, z=0),
+                    ),
+                )
+            )
+
+    shandler = _client.sequence_handler(sequence_name)
+    assert shandler is not None
+    thandler_1 = shandler.get_topic_handler(topic_name_1)
+    assert thandler_1 is not None
+    assert thandler_1.locked is True
+    thandler_2 = shandler.get_topic_handler(topic_name_2)
+    assert thandler_2 is not None
+    assert thandler_2.locked is True
+
+    # Demonstrate the push in /topic_2 has been made
+    assert thandler_2.chunks_number > 0
+
+    # free resources
+    _client.sequence_delete(sequence_name)
+    _client.close()
+
+
+def test_topic_level_error_policy_ignore(_client: MosaicoClient):
+    sequence_name = "sequence-to-delete"
+    topic_name = "/topic1"
+
+    # It is necessary to make the exception propagate until the SequenceWriter.__exit__
+    # which triggers the delete condition
+    with _client.sequence_create(
+        sequence_name, {}, on_error=SessionLevelErrorPolicy.Delete
+    ) as wseq:
+        log.info("Expected one (1) error after this line...")
+        twriter = wseq.topic_create(
+            topic_name, {}, IMU, on_error=TopicLevelErrorPolicy.Ignore
+        )
+        assert twriter is not None
+        with twriter:
+            # raise and exception to exit the context
+            raise Exception("__exception_in_test__")
+        assert twriter.status == TopicWriterStatus.IgnoredLastError
+        assert twriter.last_error == "__exception_in_test__"
+        assert twriter.is_active is True
+        # still can push data
+        twriter.push(
+            Message(
+                timestamp_ns=0,
+                data=IMU(
+                    acceleration=Vector3d(x=0, y=0, z=0),
+                    angular_velocity=Vector3d(x=0, y=0, z=0),
+                ),
+            )
+        )
+        # The last push reset the error
+        assert twriter.status == TopicWriterStatus.Active
+        assert twriter.last_error is None
+
+    shandler = _client.sequence_handler(sequence_name)
+    assert shandler is not None
+    thandler = shandler.get_topic_handler(topic_name)
+    assert thandler is not None
+    assert thandler.locked is True
+
+    # free resources
+    _client.sequence_delete(sequence_name)
+    _client.close()
+
+
+def test_topic_level_error_policy_ignore_multi_topic(_client: MosaicoClient):
+    sequence_name = "sequence-to-delete"
+    topic_name_1 = "/topic1"
+    topic_name_2 = "/topic2"
+
+    # It is necessary to make the exception propagate until the SequenceWriter.__exit__
+    # which triggers the delete condition
+    with _client.sequence_create(
+        sequence_name, {}, on_error=SessionLevelErrorPolicy.Delete
+    ) as wseq:
+        log.info("Expected one (1) error after this line...")
+        twriter_1 = wseq.topic_create(
+            topic_name_1, {}, IMU, on_error=TopicLevelErrorPolicy.Ignore
+        )
+        assert twriter_1 is not None
+        with twriter_1:
+            # raise and exception to exit the context
+            raise Exception("__exception_in_test__")
+        assert twriter_1.status == TopicWriterStatus.IgnoredLastError
+        assert twriter_1.last_error == "__exception_in_test__"
+        assert twriter_1.is_active is True
+        # still can push data
+        twriter_1.push(
+            Message(
+                timestamp_ns=0,
+                data=IMU(
+                    acceleration=Vector3d(x=0, y=0, z=0),
+                    angular_velocity=Vector3d(x=0, y=0, z=0),
+                ),
+            )
+        )
+        # The last push reset the error
+        assert twriter_1.status == TopicWriterStatus.Active
+        assert twriter_1.last_error is None
+
+        # Demonstrate topic independence
+        twriter_2 = wseq.topic_create(
+            topic_name_2, {}, IMU, on_error=TopicLevelErrorPolicy.Ignore
+        )
+        assert twriter_2 is not None
+        # still can push data
+        twriter_2.push(
+            Message(
+                timestamp_ns=0,
+                data=IMU(
+                    acceleration=Vector3d(x=0, y=0, z=0),
+                    angular_velocity=Vector3d(x=0, y=0, z=0),
+                ),
+            )
+        )
+        # The last push reset the error
+        assert twriter_2.status == TopicWriterStatus.Active
+        assert twriter_2.last_error is None
+
+    shandler = _client.sequence_handler(sequence_name)
+    assert shandler is not None
+    thandler_1 = shandler.get_topic_handler(topic_name_1)
+    assert thandler_1 is not None
+    assert thandler_1.locked is True
+    thandler_2 = shandler.get_topic_handler(topic_name_2)
+    assert thandler_2 is not None
+    assert thandler_2.locked is True
+    # Demonstrate the push in /topic_2 has been made
+    assert thandler_2.chunks_number > 0
+
+    # free resources
+    _client.sequence_delete(sequence_name)
+    _client.close()
+
+
+def test_topic_level_error_policy_raise(_client: MosaicoClient):
+    sequence_name = "sequence-to-delete"
+    topic_name = "/topic1"
+
+    # It is necessary to make the exception propagate until the SequenceWriter.__exit__
+    # which triggers the delete condition
+    with pytest.raises(
+        Exception, match="__exception_in_test__"
+    ):  # The exception bubbles up to here
+        with _client.sequence_create(
+            sequence_name, {}, on_error=SessionLevelErrorPolicy.Delete
+        ) as wseq:
+            log.info("Expected one (1) error after this line...")
+            twriter = wseq.topic_create(
+                topic_name, {}, IMU, on_error=TopicLevelErrorPolicy.Raise
+            )
+            assert twriter is not None
+            with twriter:
+                # raise and exception to exit the context
+                # must bubble up to SequenceWriter.__exit__ which deletes the data
+                raise Exception("__exception_in_test__")
+
+            # Everything here is skipped
+
+    shandler = _client.sequence_handler(sequence_name)
+    assert shandler is None
+
+    # free resources
+    _client.close()
+
+
 def test_sequence_abort(_client: MosaicoClient):
     sequence_name = "sequence-to-delete"
     topic_name = "/topic1"
@@ -98,6 +369,7 @@ def test_sequence_abort(_client: MosaicoClient):
     _client.close()
 
 
+# TODO: Delete before 0.4.0
 def test_sequence_report_deprecated_policy(_client: MosaicoClient):
     sequence_name = "sequence-to-report"
     topic_name = "/topic1"
