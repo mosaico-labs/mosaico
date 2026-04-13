0ed82d41bb93b731e263382e8361b6a3e0e22999
LuigiTer <luigi.terrone@mosaico.dev>
2026-04-13 13:58:17 +0200

    Added Futures Ontologies Adapters [#367]
    - Added override_msgs.py with Futures Ontologies Adapters.
    - Added PointCloudBaseAdapter.
    - Updated doc.

--- mosaico-sdk-py/src/mosaicolabs/ros_bridge/adapters/helpers.py
+++ mosaico-sdk-py/src/mosaicolabs/ros_bridge/adapters/helpers.py
@@ -1,4 +1,4 @@
-from typing import Type
+from typing import List, Type
 
 from ..adapter_base import ROSAdapterBase
 
@@ -23,4 +23,24 @@ def _validate_msgdata(
         raise ValueError(
             f"Malformed ROS message '{cls.ros_msgtype}': missing required keys {missing_keys}. "
             f"Available keys: {list(ros_data.keys())}"
+        )
+
+
+def _validate_required_fields(
+    cls: Type[ROSAdapterBase], required_fields: List, data: dict
+):
+    """
+    Validate that all required fields are present in the decoded data dictionary.
+
+    Args:
+        cls (Type[ROSAdapterBase]): The adapter class being validated.
+        data (dict): The decoded data dictionary to validate against.
+
+    Raises:
+        ValueError: If one or more required fields are missing from the data.
+    """
+    if not all(field in data for field in required_fields):
+        raise ValueError(
+            f"Required fields of {cls.__name__} are missing: "
+            f"Required = {cls._REQUIRED_KEYS}, Actual =  {data.keys()}"
         )
