7091c842bbd184a69313dc70dd1e04cb4879a715
Pucciland95 <niccolo.lucci@mosaico.dev>
2026-04-14 18:23:19 +0200

    wip: possible to get joint data stream from rosbag in backend

--- mosaico-sdk-py/src/mosaicolabs/examples/mujoco_vis/main.py
+++ mosaico-sdk-py/src/mosaicolabs/examples/mujoco_vis/main.py
@@ -22,9 +22,10 @@ import sys
 
 from rich.console import Console
 from rich.panel import Panel
+from rich.table import Table
 
 # Mosaico SDK Imports
-from mosaicolabs import MosaicoClient, QuerySequence
+from mosaicolabs import MosaicoClient, QuerySequence, QueryTopic, RobotJoint
 
 # Example Imports
 from ..config import (
@@ -62,39 +63,78 @@ def main():
     Executes the multi-phase Mosaico workflow.
 
     The phases are:
-    1. Connect & retrieve sequence metadata: get from backend a specific sequence.
-    2. Stream joint state topic: # TODO
+    1. Connect & retrieve sequence metadata: get from backend a specific sequence with a query.
+    2. Stream joint state topic: from query response, check that the expected data exist
     3. Plot joint trajectories: # TODO
     4. Replay in MuJoCo: # TODO
     """
 
     # --- PHASE 1: Connect & retrieve sequence metadata ---
-    # Connect to the client using a context manager to ensure resource cleanup.
+    # Connect to the client using a context manager to ensure resource cleanup + query creation.
     console.print(
         Panel(
             "[bold green]Phase 1: Connect & retrieve sequence metadata {ROBOT_SEQUENCE_NAME}[/bold green]"
         )
     )
 
+    robot_joints_timeseries: list[RobotJoint] = []
     with MosaicoClient.connect(
         host=MOSAICO_HOST,
         port=MOSAICO_PORT,
         enable_tls=ENABLE_TLS,
         api_key=API_KEY,
     ) as client:
-        result = client.query(QuerySequence().with_name(ROBOT_SEQUENCE_NAME))
+        result = client.query(
+            QuerySequence().with_name(ROBOT_SEQUENCE_NAME),
+            QueryTopic().with_ontology_tag(RobotJoint.ontology_tag()),
+        )
 
-        if result:
-            pass
-            # TODO
-
-        else:
+        if result is None:
             console.print(
                 f"[bold red] ERROR: could not find sequence called {ROBOT_SEQUENCE_NAME} [/bold red]"
             )
             console.print(
                 "[bold yellow] Please ensure you run ros_injection example (https://docs.mosaico.dev/SDK/examples/ros_injection/) before this one! [/bold yellow]"
             )
+            sys.exit(1)
+
+        # --- PHASE 2: Stream joint state topic ---
+        for items in result:
+            console.print(
+                f"[bold green] Sequence {items.sequence.name} contains {len(items.topics)} topics of type {RobotJoint.ontology_tag()} [/bold green]"
+            )
+            for topic in items.topics:
+                top_handler = client.topic_handler(items.sequence.name, topic.name)
+
+                if top_handler is None:
+                    console.print("Topic handler is None")
+                    continue
+
+                if top_handler.ontology_tag != RobotJoint.ontology_tag():
+                    console.print(
+                        f"Topic handler is not of {RobotJoint.ontology_tag()} type but of type {top_handler.ontology_tag}"
+                    )
+                    continue
+
+                rob_joints_stream = top_handler.get_data_streamer()
+
+                for joint_msg in rob_joints_stream:
+                    robot_joints_timeseries.append(joint_msg.get_data(RobotJoint))
+
+                rob_joints_stream.close()
+
+        # To begin with, visualise them on a table
+        MAX_TABLE_ROWS = 50
+        table = Table(title="Joint position values")
+        for j_name in robot_joints_timeseries[0].names:
+            table.add_column(j_name)
+
+        for rob_joint in robot_joints_timeseries[:MAX_TABLE_ROWS]:
+            table.add_row(*[f"{j:.8f}" for j in rob_joint.positions])
+
+        console.print(table)
+
+        # --- PHASE 3: TODO ---
 
 
 if __name__ == "__main__":
