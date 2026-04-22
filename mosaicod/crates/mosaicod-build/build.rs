405cad09ee9a593c0bcb852f9e7e583dc4e98c38
cabba <federico.cabassi@mosaico.dev>
2026-04-22 13:28:13 +0200

    Fixed broken docker build workflow

--- mosaicod/crates/mosaicod-build/build.rs
+++ mosaicod/crates/mosaicod-build/build.rs
@@ -2,16 +2,35 @@ use chrono::Utc;
 use std::process::Command;
 
 fn main() {
-    let profile = std::env::var("PROFILE").expect("Missing PROFILE during build.");
-    println!("cargo:rustc-env=BUILD_PROFILE={}", profile);
+    let opt_level = std::env::var("OPT_LEVEL").unwrap();
+    println!("cargo:rustc-env=MOSAICOD_BUILD_OPT_LEVEL={}", opt_level);
 
-    let sha = Command::new("git")
-        .args(["rev-parse", "HEAD"])
-        .output()
-        .expect("Failed to execute git command");
-    let git_hash = String::from_utf8(sha.stdout).unwrap();
-    println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+    let debug = std::env::var("DEBUG").unwrap();
+    println!("cargo:rustc-env=MOSAICOD_BUILD_DEBUG={}", debug);
+
+    let arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap_or_else(|_| "unknown".to_string());
+    println!(
+        "cargo:rustc-env=MOSAICOD_BUILD_ARCH={}-{}-{}",
+        os, arch, env
+    );
+
+    let profile = std::env::var("PROFILE").unwrap();
+    println!("cargo:rustc-env=MOSAICOD_BUILD_PROFILE={}", profile);
 
     let build_time = Utc::now().to_rfc2822();
-    println!("cargo:rustc-env=BUILD_TIME={}", build_time);
+    println!("cargo:rustc-env=MOSAICOD_BUILD_TIME={}", build_time);
+
+    let git_hash = std::env::var("GIT_HASH").unwrap_or_else(|_| {
+        String::from_utf8(
+            Command::new("git")
+                .args(["rev-parse", "HEAD"])
+                .output()
+                .expect("Failed to execute git command")
+                .stdout,
+        )
+        .unwrap()
+    });
+    println!("cargo:rustc-env=MOSAICOD_GIT_HASH={}", git_hash);
 }
--- mosaicod/crates/mosaicod-build/src/lib.rs
+++ mosaicod/crates/mosaicod-build/src/lib.rs
@@ -1,21 +1,30 @@
 use std::sync::OnceLock;
 
-pub const BUILD_PROFILE: &str = env!("BUILD_PROFILE");
+pub const BUILD_PROFILE: &str = env!("MOSAICOD_BUILD_PROFILE");
+pub const BUILD_OPT_LEVEL: &str = env!("MOSAICOD_BUILD_OPT_LEVEL");
+pub const BUILD_DEBUG: &str = env!("MOSAICOD_BUILD_DEBUG");
+pub const BUILD_ARCH: &str = env!("MOSAICOD_BUILD_ARCH");
+
 pub const SEMVER: &str = env!("CARGO_PKG_VERSION");
-pub const GIT_HASH: &str = env!("GIT_HASH");
-pub const BUILD_TIME: &str = env!("BUILD_TIME");
+pub const GIT_HASH: &str = env!("MOSAICOD_GIT_HASH");
+pub const BUILD_TIME: &str = env!("MOSAICOD_BUILD_TIME");
 
 static VERSION: OnceLock<String> = OnceLock::new();
 
 pub fn version_description() -> &'static str {
     VERSION.get_or_init(|| {
         format!(
-            r#"version {semver}, build {hash} ({profile})
-Built: {time}"#,
+            r#"version {semver} ({hash})
+
+Profile:  {profile}-opt{opt_level}
+Arch:     {arch} 
+Built:    {time}"#,
             semver = SEMVER,
             profile = BUILD_PROFILE,
             hash = &GIT_HASH[..8],
             time = BUILD_TIME,
+            arch = BUILD_ARCH,
+            opt_level = BUILD_OPT_LEVEL,
         )
     })
 }
