2a239a86e83712adaf82d80ea8e0476e23721415
Pucciland95 <niccolo.lucci@mosaico.dev>
2026-05-05 10:31:09 +0200

    wip(py): started adding to_ros() for tf2_msgs adapters

--- mosaico-sdk-py/src/mosaicolabs/ros_bridge/adapters/tf2_msgs.py
+++ mosaico-sdk-py/src/mosaicolabs/ros_bridge/adapters/tf2_msgs.py
@@ -1,4 +1,9 @@
-from typing import Any, Optional, Tuple, Type
+from typing import TYPE_CHECKING, Any, Optional, Tuple, Type, Union
+
+from rosbags.typesys.store import Typestore
+
+if TYPE_CHECKING:
+    from rosbags.typesys.store import MsgType
 
 from mosaicolabs.models import Message
 
@@ -6,12 +11,12 @@ from ..adapter_base import ROSAdapterBase
 from ..data_ontology import FrameTransform
 from ..ros_bridge import register_default_adapter
 from ..ros_message import ROSMessage
-from .geometry_msgs import TransformAdapter
+from .geometry_msgs import QuaternionAdapter, TransformAdapter, Vector3Adapter
 from .helpers import _validate_msgdata
 
 
 @register_default_adapter
-class FrameTransformAdapter(ROSAdapterBase):
+class FrameTransformAdapter(ROSAdapterBase[FrameTransform]):
     """
     Adapter for translating ROS TF2 messages to Mosaico `FrameTransform`.
 
@@ -125,6 +130,57 @@ class FrameTransformAdapter(ROSAdapterBase):
                 for ros_transf_dict in list(ros_data["transforms"])
             ],
         )
+
+    @classmethod
+    def to_ros(
+        cls,
+        mosaico_data: Union[Message, FrameTransform],
+        typestore: Typestore,
+        input_ros_msg_type: Optional[str] = None,
+    ) -> "Optional[MsgType]":
+        """
+        TODO
+        """
+
+        # Resolve ROS message to translate Mosaico message to if not defined in input
+        resolved_rosmsg_type = input_ros_msg_type or cls.get_default_ros_msg()
+        if not cls.is_rosmsg_type_valid(resolved_rosmsg_type):
+            return None
+
+        # Checking presence in typestore of requested message
+        if typestore.types.get(resolved_rosmsg_type) is None:
+            return None
+
+        # Unpacking Mosaico message / type
+        frame_transform_data, header = cls.unpack_mosaico_msg(mosaico_data)
+
+        # Filling the data
+        RosTFMessage = typestore.types["tf2_msgs/msg/TFMessage"]
+        RosTransform = typestore.types["geometry_msgs/msg/Transform"]
+        RosTransformStamped = typestore.types["geometry_msgs/msg/TransformStamped"]
+
+        # TODO: is there a better way to handle this?
+        tf_transforms = []
+        for transform_data in frame_transform_data.transforms:
+            ros_transform = RosTransform(
+                translation=Vector3Adapter.to_ros(
+                    transform_data.translation, typestore
+                ),
+                rotation=QuaternionAdapter.to_ros(transform_data.rotation, typestore),
+            )
+
+            ros_transform_stamped = RosTransformStamped(
+                header=header.to_ros(),
+                child_frame_id=transform_data.target_frame_id,
+                transform=ros_transform,
+            )  # TODO: how to handle child_frame_id? Is target_frame_id the same thing?
+
+            tf_transforms.append(ros_transform_stamped)
+
+        if resolved_rosmsg_type == "tf2_msgs/msg/TFMessage":
+            return RosTFMessage(transforms=tf_transforms)
+
+        return None
 
     @classmethod
     def schema_metadata(cls, ros_data: dict, **kwargs: Any) -> Optional[dict]:
