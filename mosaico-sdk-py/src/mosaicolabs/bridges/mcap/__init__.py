4c3590410a87e03e74501d85e421ece0c24781fb
Pucciland95 <niccolo.lucci@mosaico.dev>
2026-09-11 13:30:22 +0200

    feat(mcap): add initial implementation of MCAP bridge module

---
+++ mosaico-sdk-py/src/mosaicolabs/bridges/mcap/__init__.py
@@ -0,0 +1,7 @@
+from . import adapters as adapters
+from .adapter_base import MCAPAdapterBase as MCAPAdapterBase
+from .bridge import (
+    MCAPBridge as MCAPBridge,
+    compute_mcap_msg_type as compute_mcap_msg_type,
+    register_default_adapter as register_default_adapter,
+)
