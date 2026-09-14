04ef1a040fbf465d9686a3e760059b4f55d12d0f
Pucciland95 <niccolo.lucci@mosaico.dev>
2026-09-14 15:17:06 +0200

    refactor: update create_mcap_injection_config to include host and port parameters

--- mosaico-sdk-py/src/testing/integration/bridges/mcap/test_injector.py
+++ mosaico-sdk-py/src/testing/integration/bridges/mcap/test_injector.py
@@ -3,11 +3,15 @@ from pathlib import Path
 from mosaicolabs.bridges.mcap import MCAPInjectionConfig, MCAPInjector
 
 
-def create_mcap_injection_config(mcap_path: Path, sequence_name: str, dry_run=False):
+def create_mcap_injection_config(
+    mcap_path: Path, sequence_name: str, host: str, port: int, dry_run=False
+):
     return MCAPInjectionConfig(
         file_path=mcap_path,
         sequence_name=sequence_name,
         metadata={},
+        host=host,
+        port=port,
         dry_run=dry_run,
     )
 
@@ -22,6 +26,8 @@ def test_mcap_injection_succeds(
     mcap_injection_config_proto = create_mcap_injection_config(
         Path(mcap_protobuf_file),
         sequence_name_proto,
+        host=mosaico_client._host,
+        port=mosaico_client._port,
     )
     MCAPInjector(mcap_injection_config_proto).run()
 
@@ -46,6 +52,8 @@ def test_mcap_injection_succeds(
     mcap_injection_config_protojson = create_mcap_injection_config(
         Path(mcap_mixed_file),
         sequence_name_mixed,
+        host=mosaico_client._host,
+        port=mosaico_client._port,
     )
     MCAPInjector(mcap_injection_config_protojson).run()
 
