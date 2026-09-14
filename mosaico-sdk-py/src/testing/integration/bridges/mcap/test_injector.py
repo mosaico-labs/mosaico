5857e0891d1b32f28aa6e9a1f790ec0d7041a82e
Pucciland95 <niccolo.lucci@mosaico.dev>
2026-09-14 18:06:37 +0200

    refactor: update timeout parameter in MosaicoClient and add new fixtures for MCAP injection tests

--- mosaico-sdk-py/src/testing/integration/bridges/mcap/test_injector.py
+++ mosaico-sdk-py/src/testing/integration/bridges/mcap/test_injector.py
@@ -1,62 +1,38 @@
-from pathlib import Path
-
 from mosaicolabs.bridges.mcap import MCAPInjectionConfig, MCAPInjector
 
 
-def create_mcap_injection_config(
-    mcap_path: Path, sequence_name: str, host: str, port: int, dry_run=False
-):
-    return MCAPInjectionConfig(
-        file_path=mcap_path,
-        sequence_name=sequence_name,
-        metadata={},
-        host=host,
-        port=port,
-        dry_run=dry_run,
-    )
-
-
-# TODO: FIX THIS
-def _test_mcap_injection_succeds(
-    mcap_protobuf_file, mcap_jsonschema_file, mcap_mixed_file, mosaico_client
+def test_mcap_injection_succeds(
+    mosaico_client,
+    default_mcap_protobuf_injector_config: MCAPInjectionConfig,
+    # default_mcap_jsonschema_injector_config: MCAPInjectionConfig,
+    default_mcap_mixed_injector_config: MCAPInjectionConfig,
 ):
     """Tests that the whole ingestion pipeline works using all available sample mcap files"""
 
     # 1) Protobuf only mcap
-    sequence_name_proto = Path(mcap_protobuf_file).stem
-    mcap_injection_config_proto = create_mcap_injection_config(
-        Path(mcap_protobuf_file),
-        sequence_name_proto,
-        host=mosaico_client._host,
-        port=mosaico_client._port,
-    )
-    MCAPInjector(mcap_injection_config_proto).run()
+    MCAPInjector(default_mcap_protobuf_injector_config).run()
 
-    assert mosaico_client.sequence_exists(sequence_name_proto) is True
-    mosaico_client.sequence_delete(sequence_name_proto)
+    assert (
+        mosaico_client.sequence_exists(
+            default_mcap_protobuf_injector_config.sequence_name
+        )
+        is True
+    )
+    mosaico_client.sequence_delete(default_mcap_protobuf_injector_config.sequence_name)
 
     # 2) jsonschema only mcap
-    # sequence_name_json = Path(mcap_jsonschema_file).stem
-    # mcap_injection_config_json = create_mcap_injection_config(
-    #     Path(mcap_jsonschema_file),
-    #     sequence_name_json,
-    #     host=mosaico_client._host,
-    #     port=mosaico_client._port,
-    # )
-    # MCAPInjector(mcap_injection_config_json).run()
+    # default_mcap_jsonschema_injector_config.sequence_name = Path(mcap_jsonschema_file).stem
+    # MCAPInjector(default_mcap_jsonschema_injector_config).run()
 
-    # assert mosaico_client.sequence_exists(sequence_name_json) is True
-    # mosaico_client.sequence_delete(sequence_name_json)
+    # assert mosaico_client.sequence_exists(default_mcap_jsonschema_injector_config.sequence_name) is True
+    # mosaico_client.sequence_delete(default_mcap_jsonschema_injector_config.sequence_name)
 
     # 3) protobuf + jsonschema mcap
-    sequence_name_mixed = Path(mcap_mixed_file).stem
-    mcap_injection_config_protojson = create_mcap_injection_config(
-        Path(mcap_mixed_file),
-        sequence_name_mixed,
-        host=mosaico_client._host,
-        port=mosaico_client._port,
-    )
-    MCAPInjector(mcap_injection_config_protojson).run()
+    MCAPInjector(default_mcap_mixed_injector_config).run()
 
-    assert mosaico_client.sequence_exists(sequence_name_mixed) is True
-    mosaico_client.sequence_delete(sequence_name_mixed)
+    assert (
+        mosaico_client.sequence_exists(default_mcap_mixed_injector_config.sequence_name)
+        is True
+    )
+    mosaico_client.sequence_delete(default_mcap_mixed_injector_config.sequence_name)
+    mosaico_client.close()
